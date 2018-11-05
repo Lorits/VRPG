388df4e35d738726e7ea8e756e3f1a6c4bafdd05
speered <caspertaccopiv@gmail.com>
2018-11-05 16:33:12 +0100

    Added better magnifying glass script
    Basically what the title is. Nothing more has been edited yet.

--- Assets/Speers/Scripts/MagnifyingGlassScript.cs
+++ Assets/Speers/Scripts/MagnifyingGlassScript.cs
@@ -17,28 +17,40 @@ public class MagnifyingGlassScript : MonoBehaviour {
 
 		float angle = 0;
 		float alpha = 0;
+		bool positive = false;
 
-		float maxAngle = Mathf.Cos(30);
+		float maxAngle = Mathf.Cos(Mathf.PI/5);
 
+		Vector3 par = transform.parent.transform.localPosition;
+		
+		//Check all lights
 		foreach(GameObject light in lights){
-			Vector3 dir = (light.transform.position - transform.position).normalized;
-			Debug.Log(dir);
-			if(angle < Mathf.Max(maxAngle, Vector3.Dot(dir,transform.forward))){
-				angle = Mathf.Max(maxAngle, Vector3.Dot(dir,transform.forward));
+			Vector3 dir = (light.transform.position - transform.parent.transform.position).normalized;
+			
+			//Check whether positive or negative
+			if(Vector3.Dot(dir,transform.forward) > 0) positive = true;
+
+			if(angle < Mathf.Max(maxAngle, Mathf.Abs(Vector3.Dot(dir,transform.forward)))){
+				angle = Mathf.Max(maxAngle, Mathf.Abs(Vector3.Dot(dir,transform.forward)));
 			}
 		}
 
-		alpha = 1/(1 - maxAngle)*(angle - maxAngle); 
+		alpha = 1/(1 - maxAngle + 0.05f)*(angle - maxAngle); 
 
-		if(alpha > 0){
-			
-			MeshRenderer.enabled = true;
+		Debug.Log(maxAngle + " & " + angle + " & " + alpha + " & " + positive);
+
+		//Change position and scale so it looks more like a ray.
 		
+		if(angle > maxAngle && positive){
+			transform.position = new Vector3(par.x,par.y,(positive ? (-alpha):(alpha)));
+			transform.localScale = new Vector3(transform.localScale.x,transform.localScale.y, (alpha)*10);
+			
+			gameObject.GetComponent<MeshRenderer>().enabled = true;
 			Color col = gameObject.GetComponent<Renderer>().material.color;
-			col.a = alpha - Mathf.Cos(35);
+			col.a = alpha;
 			gameObject.GetComponent<Renderer>().material.color = col;
 		}else{
-			MeshRenderer.enabled = false;
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
 		}
 	}
 }
